315c52c90e652135b49cd2f83b04032c60fd071d
Felipe  OG <17603069+felipeog@users.noreply.github.com>
2023-12-02 21:24:41 +0000

    Add env vars

--- infra/database.js
+++ infra/database.js
@@ -1,11 +1,11 @@
 import { Client } from "pg";
 
 const client = new Client({
-  host: "localhost",
-  port: 5432,
-  user: "postgres",
-  database: "postgres",
-  password: "local_password",
+  database: process.env.POSTGRES_DB,
+  host: process.env.POSTGRES_HOST,
+  password: process.env.POSTGRES_PASSWORD,
+  port: process.env.POSTGRES_PORT,
+  user: process.env.POSTGRES_USER,
 });
 
 async function query(queryObject) {
